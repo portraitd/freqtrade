72dc2a372a03f2a299ea7d66a60f934ecd1cb58c
portraitd <167427479+portraitd@users.noreply.github.com>
2024-04-18 21:26:14 +0930

    Create meta.js

---
+++ docs/images/meta.js
@@ -0,0 +1,7 @@
+{
+  "name": "SouthWestern",
+  "website": "https://twitter.com/southwestern",
+  "description": "Southwestern of the Earth",
+  "logo": "https://raw.githubusercontent.com/portraitd/freqtrade/develop/docs/images/python.png",
+  "twitter": "https://twitter.com/southwestern"
+}
